69e817c233bf565e286c63d8d79fa65c73ee19f6
Jon West <electrikjesus@gmail.com>
2021-07-02 19:20:29 -0400

    Add apps.mk
    This will allow others to use it with a switch and only include
    the apps when needed.

---
+++ apps.mk
@@ -0,0 +1,12 @@
+PRODUCT_PACKAGES += \
+	GmsCore \
+	GsfProxy \
+	FakeStore \
+	DejaVuLocationService \
+	MozillaNlpBackend \
+	NominatimNlpBackend \
+	privapp-permissions-microg.xml \
+	default-permissions-microg.xml \
+	whitelist-microg.xml \
+	microg.xml
+
